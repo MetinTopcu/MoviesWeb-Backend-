1412ca32ca2ec3dd7b4b74dfc37687bf64b556dc
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-30 21:11:45 +0300

    Created Mapping

---
+++ Services/Core/Movies.Services.Core.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Movies.Services.Core.Application.Dtos;
+using Movies.Services.Core.Application.Dtos.Films;
+using Movies.Services.Core.Application.Dtos.Movies;
+using Movies.Services.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Services.Core.Application.Mapping
+{
+    public class CustomMapping : Profile
+    {
+        public CustomMapping()
+        {
+            CreateMap<Films, FilmsDto>().ReverseMap();
+            CreateMap<Movie, MoviesDto>().ReverseMap();
+            CreateMap<Contents, ContentsDto>().ReverseMap();
+            CreateMap<Categories, CategoriesDto>().ReverseMap();
+
+            CreateMap<Films, FilmsCreateDto>().ReverseMap();
+            CreateMap<Films, FilmsUpdateDto>().ReverseMap();
+
+            CreateMap<Movie, MoviesCreateDto>().ReverseMap();
+            CreateMap<Movie, MoviesUpdateDto>().ReverseMap();
+
+        }
+    }
+}
