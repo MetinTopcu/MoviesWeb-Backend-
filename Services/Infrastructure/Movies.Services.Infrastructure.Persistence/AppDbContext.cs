b3300c3450a52b724d5488c89c70079f49942573
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-12-01 11:53:43 +0300

    Created DbContext

---
+++ Services/Infrastructure/Movies.Services.Infrastructure.Persistence/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Services.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Services.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext //DbContext ile EF den yararlanıyoruz.
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Films> Films { get; set; }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Categories> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
