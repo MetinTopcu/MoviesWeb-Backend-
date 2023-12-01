c970c8e6381596327b0c5f04727f1145c49bfc86
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-12-01 15:54:33 +0300

    CreatedGetById

---
+++ Services/Infrastructure/Movies.Services.Infrastructure.Persistence/CQRS/Queries/GetById/GetByIdMovieQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Movies.Services.Core.Application.Dtos.Movies;
+using Movies.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Services.Infrastructure.Persistence.CQRS.Queries.GetById
+{
+    public class GetByIdMovieQuery : IRequest<ResponseDto<MoviesDto>>
+    {
+        public string Id { get; set; }
+    }
+}
