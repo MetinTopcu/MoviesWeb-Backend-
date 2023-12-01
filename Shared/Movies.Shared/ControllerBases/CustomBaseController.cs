5a2860da17e21fe2f89cf85dcf032a2d7273a0a3
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-12-01 14:03:55 +0300

    Created Shared

---
+++ Shared/Movies.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Shared.ControllerBases
+{
+    public class CustomBaseController : ControllerBase
+    {
+
+        public IActionResult CreateActionResultInstance<T>(ResponseDto<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+
+    }
+}
