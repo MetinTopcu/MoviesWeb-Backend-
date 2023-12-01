f91fba29d8daf0cc2c4a171c240c131527d18d13
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-12-01 15:54:25 +0300

    Created GetAll

---
+++ Services/Infrastructure/Movies.Services.Infrastructure.Persistence/CQRS/Queries/GetAll/GetAllCategoryQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Movies.Services.Core.Application.Dtos;
+using Movies.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Services.Infrastructure.Persistence.CQRS.Queries.GetAll
+{
+    public class GetAllCategoryQuery : IRequest<ResponseDto<CategoriesDto>>
+    {
+
+    }
+}
---
+++ Services/Infrastructure/Movies.Services.Infrastructure.Persistence/CQRS/Queries/GetAll/GetAllFilmQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Movies.Services.Core.Application.Dtos.Films;
+using Movies.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Services.Infrastructure.Persistence.CQRS.Queries.GetAll
+{
+    public class GetAllFilmQuery : IRequest<ResponseDto<FilmsDto>>
+    {
+    }
+}
